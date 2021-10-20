a3a74c06db843eaee01fe330ab31d4cbcaa14b53
JustinDMcGraw <drslaughterrex@gmail.com>
2021-10-20 15:16:53 -0500

    Started CSS of Rows

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios.js";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -21,7 +22,11 @@ function Row({ title, fetchUrl }) {
 
       <div className="row_posters">
         {movies.map((movie) => (
-          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+          <img
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
         ))}
       </div>
     </div>
